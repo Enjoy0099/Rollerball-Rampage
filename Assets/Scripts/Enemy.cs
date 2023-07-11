9a1c9d88763ed4322d05688ae4647067055c5a0f
Trada Kevin <tradakevin0099@gmail.com>
2023-07-11 10:32:25 +0530

    Make a method return a spawn point of Enemy
    Enemy spawns at random location on the island
    Enemy follows the player around
    Spheres bounce off of each other
    
    Physics Materials
    Defining vectors in 3D space
    Normalizing values
    Methods with return values

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    public float enemySpeed = 5f;
+    private Rigidbody enemyBody;
+    private GameObject player;
+
+    private void Start()
+    {
+        enemyBody = GetComponent<Rigidbody>();
+        player = GameObject.FindWithTag(NameManager.PLAYER_TAG);
+    }
+
+    private void Update()
+    {
+        Vector3 lookDirection = (player.transform.position - transform.position).normalized;
+
+        enemyBody.AddForce(lookDirection * enemySpeed * Time.deltaTime, ForceMode.Impulse);  
+    }
+}
--- Assets/Scripts/NameManager.cs
+++ Assets/Scripts/NameManager.cs
@@ -3,6 +3,8 @@ public static class NameManager
     public static string HORIZONTAL_INPUT = "Horizontal";
     public static string VERTICAL_INPUT = "Vertical";
 
+    public static string PLAYER_TAG = "Player";
+    public static string ENEMY_TAG = "Enemy";
     public static string FOCALPOINT_TAG = "FocalPoint";
 
 }
